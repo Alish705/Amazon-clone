534f545e2b70155864092bbd8e3bae47146f5ebf
Alish705 <alishawal705@gmail.com>
2021-01-07 19:52:48 +1100

    header class created

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,26 @@ import "./Header.css";
 function Header() {
     return (
         <div className="header">
-            
+        <img className="header__logo" 
+        src ="https://logos-world.net/wp-content/uploads/2020/04/Amazon-Logo.png" 
+        alt= "amazon logo"   
+        />
+        <div className="header__search">
+            <input className="header__searchInput" type="text" />
+            {/*Logo*/}
+        </div> 
+
+        <div className="header__nav">
+            <div className="header__option">
+
+            </div>
+            <div className="header__option">
+                
+            </div>
+            <div className="header__option">
+                
+            </div>
+        </div>           
         </div>
     )
 }
