2812195ec99f2dab6f6316e7ce0227a7ac62f77e
Alish705 <alishawal705@gmail.com>
2021-01-13 13:17:49 +1100

    authentication done

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,19 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBJp1-B3bw8jCLA8vPNWPNgNyBFtMeNgAA",
-    authDomain: "clone-85deb.firebaseapp.com",
-    projectId: "clone-85deb",
-    storageBucket: "clone-85deb.appspot.com",
-    messagingSenderId: "943058345337",
-    appId: "1:943058345337:web:6e54362950799d015b23a8",
-    measurementId: "G-GWF8JBN66Y"
-  };
+	apiKey: "AIzaSyBJp1-B3bw8jCLA8vPNWPNgNyBFtMeNgAA",
+	authDomain: "clone-85deb.firebaseapp.com",
+	projectId: "clone-85deb",
+	storageBucket: "clone-85deb.appspot.com",
+	messagingSenderId: "943058345337",
+	appId: "1:943058345337:web:6e54362950799d015b23a8",
+	measurementId: "G-GWF8JBN66Y",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
